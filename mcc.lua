c0dc8c544f6bc4086551a9e5497603d730fd3fdf
Nadya Djojosantoso <hello@nadyafebi.com>
2022-10-03 21:06:58 -0700

    Add uninstall command to mcc

--- mcc.lua
+++ mcc.lua
@@ -5,6 +5,7 @@ local function printUsage()
     print("Get a script from Meeu's repo")
     print("Usage:")
     print("mcc install <script>")
+    print("mcc uninstall <script>")
 end
 
 local function getFromRepo(path)
@@ -17,10 +18,18 @@ local function getFromRepo(path)
     end
 end
 
-local function runInstall(path)
+local function appendLuaToPath(path)
     if string.sub(path, -4) ~= ".lua" then
         path = path .. ".lua"
     end
+end
+
+local function getProgramName(path)
+    return path:match(".*\/(.*)\.lua$")
+end
+
+local function runInstall(path)
+    path = appendLuaToPath(path)
 
     local filePath = shell.resolve(programRoot .. path)
     if fs.exists(filePath) then
@@ -29,21 +38,37 @@ local function runInstall(path)
 
     local result = getFromRepo(path)
     if result then
-        local file = fs.open(path, "w")
+        local file = fs.open(filePath, "w")
         file.write(result)
         file.close()
     else
-        print("Could not download file")
+        error("Could not download file")
     end
 
-    local programName = path:match(".*\/(.*)\.lua$")
+    local programName = getProgramName(path)
     shell.setAlias(programName, filePath)
+end
+
+local function runUninstall(path)
+    path = appendLuaToPath(path)
+
+    local filePath = shell.resolve(programRoot .. path)
+    if fs.exists(filePath) then
+        fs.delete(filePath)
+    else
+        error("Program to uninstall not found")
+    end
+
+    local programName = getProgramName(path)
+    shell.clearAlias(programName)
 end
 
 local args = { ... }
 
 if args[1] == "install" or args[1] == "i" and #args == 2 then
     runInstall(args[2])
+elseif args[1] == "uninstall" and #args == 2 then
+    runUninstall(args[2])
 else
     printUsage()
 end
