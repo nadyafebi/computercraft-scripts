8b6a16ed9c5a430fce387c8a00f0d27c3ed50443
Nadya Djojosantoso <hello@nadyafebi.com>
2022-10-03 21:19:46 -0700

    Add floor

---
+++ turtle/floor.lua
@@ -0,0 +1,47 @@
+local function printUsage()
+    print("Place floor in a room")
+    print("Usage:")
+    print("floor <w> <l>")
+end
+
+local args = { ... }
+if #args ~= 2 then
+    printUsage()
+    return
+end
+
+local w, l = tonumber(args[1]) or nil, tonumber(args[2]) or nil
+
+local selectedSlot = 1
+turtle.select(selectedSlot)
+
+for i = 1, w do
+    for j = 1, l do
+        if turtle.getItemCount() == 0 then
+            selectedSlot = selectedSlot + 1
+            if selectedSlot > 16 then
+                return i, "No items left in turtle"
+            end
+            turtle.select(selectedSlot)
+        end
+        
+        turtle.digDown()
+        turtle.placeDown()
+
+        if j < l then
+            if turtle.detect() then
+                error("Turtle obstructed")
+            end
+            turtle.forward()
+        end
+    end
+
+    if i < w then
+        turtle.turnRight()
+        if turtle.detect() then
+            error("Turtle obstructed")
+        end
+        turtle.forward()
+        turtle.turnRight()
+    end
+end
